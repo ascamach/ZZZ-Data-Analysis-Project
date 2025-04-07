5e3ae0ce438f67fe8fddafe9cc64395e38dd6d1c
ascamach <ascamach@ucsc.edu>
2025-04-07 04:38:42 -0700

    Set base frame for Python scripts

---
+++ Python_Files/dmg_calcs.py
@@ -0,0 +1,2 @@
+import teammate_calcs
+
---
+++ Python_Files/teammate_calcs.py
@@ -0,0 +1,7 @@
+class teammate:
+    def __init__(self, atk_buff, dmg_bonus, pen_ratio_bonus, def_shred):
+        self.atk_buff = atk_buff
+        self.dmg_bonus = dmg_bonus
+        self.pen_ratio_bonus = pen_ratio_bonus
+        self.def_shred = def_shred
+
