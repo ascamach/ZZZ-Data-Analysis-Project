611f856af8898462923cd8f3e5c300650f23f0f2
ascamach <ascamach@ucsc.edu>
2025-04-11 09:25:46 -0700

    Add queries.md file and playing around with PANDAS

--- Python_Files/test.py
+++ Python_Files/test.py
@@ -1,3 +1,11 @@
 import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('data.csv')
+
+df["team"] = df["teammate_1"] + "_" + df["teammate_2"]
+
+plot = df.groupby("team").plot(title="Scores", x='run_number',y='damage_score',kind='line')
+
+plt.show()
---
+++ queries.md
@@ -0,0 +1,15 @@
+# SQL Queries
+
+## Query for top 5 scores in Grace/Rina team
+SELECT 
+  build_id, 
+  run_number, 
+  teammate_1, 
+  teammate_2, 
+  damage_score 
+WHERE 
+  teammate_1 == "grace" 
+  AND teammate_2 == "rina" 
+ORDER BY 
+  damage_score DESC 
+LIMIT 5
